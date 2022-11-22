fabd8c0329da29c88495273899edd5a04cab8856
klaudorowicz <k.laudorowicz@gmail.com>
2022-11-22 16:34:30 +0100

    Created flex whiteboard / worksheet with stretch flex divs

--- script.js
+++ script.js
@@ -2,15 +2,37 @@
 
 // Query Selectors and Altering Elements
 const paintContainer = document.querySelector('.paintContainer');
-const divInPaint = document.createElement('div');
 const setResolutionBtn = document.querySelector('#setResolutionBtn');
 let sheetSize = 16;
-
+let sheetSize2 = 16* 16;
 
 // Set Resolution Button
-  setResolutionBtn.addEventListener('click', function(e) {
+  setResolutionBtn.addEventListener('click', function() {
   sheetSize = prompt('Size of worksheet:', 16);
+  sheetSize2 = sheetSize * sheetSize;
 });
 
+/* function sizeArray() {
+  let sheetSizeArray = [];
+  for(let i = 0; i < sheetSize; i++) {
+  sheetSizeArray.push('');
+}}; */
+
+let num = 0;
+
+// Create Worksheet
+// paintContainer.setAttribute('grid-template-columns', auto);
+for (let i = 0; i < sheetSize; i++) { 
+  let rowInPaint = document.createElement('div');
+  rowInPaint.classList.add('rows');
+  paintContainer.appendChild(rowInPaint);
+  for(let j = 0; j < sheetSize; j++) {
+    num += 1;
+    let divInPaint = document.createElement('div');
+    divInPaint.innerHTML = '';
+    divInPaint.className = `cell`;
+    rowInPaint.appendChild(divInPaint);
+  }
+};
 
 
